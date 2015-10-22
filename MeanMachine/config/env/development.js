56471c36cebf61ebbb4d213d82e5b8833ba29e7b
ColinFletch <cfletch@us.ibm.com>
2015-10-22 16:21:02 -0400

    First Commit

---
+++ MeanMachine/config/env/development.js
@@ -0,0 +1,14 @@
+module.exports = {
+    
+    //Dev Config Options
+    db: 'mongodb://localhost/mean-book',
+    
+    sessionSecret: 'developmentSessionSecret',
+    
+    facebook: {
+    
+    clientID:  487344474769896,
+    clientSecret: 'c5be05af840df76bb1d586100b839582',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+}
+};
